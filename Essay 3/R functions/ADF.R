64d3df0304559d495021d5b3a08f870ade952783
denisbieri <denis.bieri@hslu.ch>
2022-12-29 08:04:32 +0100

    Update

--- Essay 3/R functions/ADF.R
+++ Essay 3/R functions/ADF.R
@@ -4,7 +4,7 @@ ADF <- function(var1, data){
   dt <- dt[cumsum(complete.cases(dt)) != 0, ]
   dt <- log(dt)
   dt <- dt %>% drop_na
-  summary(ur.df(as.matrix(dt), selectlags = "AIC", type="trend", lags=10))
+  summary(ur.df(as.matrix(dt), selectlags = "AIC", type="drift", lags=10))
 }
 
 ADF_diff <- function(var1, data){
@@ -12,5 +12,5 @@ ADF_diff <- function(var1, data){
   dt <- dt[cumsum(complete.cases(dt)) != 0, ]
   dt <- dt %>% drop_na
   dt <- diff(as.matrix(log(dt)))
-  summary(ur.df(dt, selectlags = "AIC", type="trend", lags=10))
+  summary(ur.df(dt, selectlags = "AIC", type="drift", lags=10))
 }
